4761aa88d5f81b0b2107a829a8c05de96cb40f7d
reactive-firewall <reactive-firewall@users.noreply.github.com>
2018-04-17 20:54:28 -0700

    #86 - partial regresion fix

--- PiAP/opt/PiAP/bin/virus_scan.bash
+++ PiAP/opt/PiAP/bin/virus_scan.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
-SCAN_OPTIONS="-i --detect-pua=yes --heuristic-scan-precedence=yes --partition-intersection=yes --block-macros=yes --exclude-dir=/proc/ --exclude-dir=/sys/ --exclude-dir=/dev/ --exclude-dir=\"/lost+found/\" -r /"
+SCAN_OPTIONS=$"-i --detect-pua=yes --heuristic-scan-precedence=yes --partition-intersection=yes --block-macros=yes --exclude-dir=/proc/ --exclude-dir=/sys/ --exclude-dir=/dev/ --exclude-dir=\"/lost+found/\" -r /"
 SCAN_COMMAND=$(which clamscan)
 SCAN_LOG_PATH="/srv/PiAP/cache/virus_scan.log"
 (sudo nice -n -5 nohup timeout --kill-after=4m 55m "${SCAN_COMMAND}" $SCAN_OPTIONS | tee "${SCAN_LOG_PATH}" 2>/dev/null >/dev/null ) & disown
