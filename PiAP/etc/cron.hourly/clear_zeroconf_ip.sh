691df8b4f0ba0e77a5cc8a047dd0a7d5b167244d
reactive-firewall <reactive-firewall@users.noreply.github.com>
2018-10-09 22:55:58 -0700

    FEATURE - improved client zeroing feature

--- PiAP/etc/cron.hourly/clear_zeroconf_ip.sh
+++ PiAP/etc/cron.hourly/clear_zeroconf_ip.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-for IFACE in lan0 dmz0 br0 eth0 eth1 eth2 eth3 eth4 wlan0 wlan1 wlan2 wlan3 wlan4 vnet0 ; do
-        if [[ ( -n $(echo -n $(ip addr show ${IFACE:-eth0} 2>/dev/null | fgrep 169.254 | grepCIDR | grepCIDR -m1) ) ) ]] ; then
-                ip addr del $(ip addr show ${IFACE:-eth0} 2>/dev/null | fgrep 169.254 | grepCIDR | grepCIDR -m1) dev ${IFACE:-eth0} 2>/dev/null || true
-        fi
-        if [[ ( -n $(echo -n $(ip route show default 2>/dev/null | fgrep default | fgrep ${IFACE}) ) ) && ( -z $(echo -n $(ip route show ${IFACE:-eth0} 2>/dev/null | fgrep default | fgrep ${IFACE:-eth0} | fgrep src) ) ) ]] ; then
-                ip route change $(echo -n $(ip route show default 2>/dev/null | fgrep default | fgrep ${IFACE})) src $(echo -n $(ip addr show ${IFACE:-eth0} 2>/dev/null | fgrep inet | tr -s ' ' ' ' | cut -d \   -f 3 | cut -d / -f 1)) 2>/dev/null || true
-        fi
+for IFACE in lan0 br0 eth0 eth0.26 eth0.4 eth0.11 eth0.40 wlan0 wlan1 wlan2 wlan3 vnet0 eth1 eth1.40 mon0 ; do
+	if [[ ( -n $(echo -n $(ip addr show ${IFACE:-eth0} 2>/dev/null | fgrep 169.254 | grepCIDR | grepCIDR -m1) ) ) ]] ; then
+	        ip addr del $(ip addr show ${IFACE:-eth0} 2>/dev/null | fgrep 169.254 | grepCIDR | grepCIDR -m1) dev ${IFACE:-eth0} 2>/dev/null || true
+	fi
+	if [[ ( -n $(echo -n $(ip route show default 2>/dev/null | fgrep default | fgrep ${IFACE}) ) ) && ( -z $(echo -n $(ip route show ${IFACE:-eth0} 2>/dev/null | fgrep default | fgrep ${IFACE:-eth0} | fgrep src) ) ) ]] ; then
+	        ip route change $(echo -n $(ip route show default 2>/dev/null | fgrep default | fgrep ${IFACE})) src $(echo -n $(ip addr show ${IFACE:-eth0} 2>/dev/null | fgrep inet | tr -s ' ' ' ' | cut -d \   -f 3 | cut -d / -f 1)) 2>/dev/null || true
+	fi
 done
 
 exit 0 ;
